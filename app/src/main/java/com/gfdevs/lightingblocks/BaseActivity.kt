6c0a7cc44752e0ad022b596308a40b66316ae4be
Grzegorz Fraczyk <greg@Grzegorzs-MacBook-Pro.local>
2020-03-05 21:55:06 +0100

    Initial commit

--- app/src/main/java/com/gfdevs/lightingblocks/BaseActivity.kt
+++ app/src/main/java/com/gfdevs/lightingblocks/BaseActivity.kt
@@ -16,6 +16,7 @@ open class BaseActivity : AppCompatActivity() {
     override fun onStart() {
         Log.d(TAG, "${javaClass.simpleName} OnStart")
         super.onStart()
+
     }
 
     override fun onPause() {
