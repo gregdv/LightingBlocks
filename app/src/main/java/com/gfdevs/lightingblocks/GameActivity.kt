f2c601aee4fae9fc8fcb0dc0daccda0c8e76fceb
greg <grzegorz.fraczyk1@gmail.com>
2020-04-07 23:56:00 +0200

    game board added

---
+++ app/src/main/java/com/gfdevs/lightingblocks/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.gfdevs.lightingblocks
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_game.*
+
+class GameActivity : AppCompatActivity() {
+
+
+    }
+
+
